664c2a17ddf89036c4ea9ab910d763d557cb6790
SugandaSu <arifsuganda.su@gmail.com>
2017-04-16 21:50:27 +0800

    Delete isLewat.m

--- isLewat.m
+++
@@ -1,9 +0,0 @@
-function tLewat = isLewat(sKeadaan, keadaan)
-
-jKeadaan = size(sKeadaan, 1);
-tLewat = false;
-for i = 1: jKeadaan
-    if (keadaan(1) == sKeadaan(i,1)) && (keadaan(2) == sKeadaan(i,2))
-        tLewat = true;
-    end
-end
