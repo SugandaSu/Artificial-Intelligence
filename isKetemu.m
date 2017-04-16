9b53e9cfbdc93afb1244778b46afdf7df0edd5b0
SugandaSu <arifsuganda.su@gmail.com>
2017-04-16 21:49:21 +0800

    Delete isKetemu.m

--- isKetemu.m
+++
@@ -1,7 +0,0 @@
-function ketemu = isKetemu(sKeadaan, tujuan)
-
-jKeadaan = size(sKeadaan,1);
-ketemu = false;
-if (sKeadaan(jKeadaan,1) == tujuan) || (sKeadaan(jKeadaan,2) == tujuan)
-    ketemu = true;
-end
